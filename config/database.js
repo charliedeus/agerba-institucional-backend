0fad7e150e57e428a99e3602de2a080de0a5bd26
charliedeus <charliedeus@gmail.com>
2022-08-17 15:12:30 -0300

    fix connection db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,16 @@
 module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'agerba-db'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', '852456951357'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+
   // connection: {
   //   client: 'postgres',
   //   connection: {
@@ -10,33 +22,4 @@ module.exports = ({ env }) => ({
   //     ssl: env.bool('DATABASE_SSL', false),
   //   },
   // },
-
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST', '127.0.0.1'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'agerba-db'),
-          user: env('DATABASE_USERNAME', 'docker'),
-          password: env('DATABASE_PASSWORD', 'docker'),
-          ssl: env.bool('DATABASE_SSL', false),
-        },
-      },
-    },
-    production: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "agerba-db"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "852456951357"),
-      },
-      options: {},
-    },
-  },
 });
