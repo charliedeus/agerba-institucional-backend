c9231350a458ff29032fe75fb60211dd2df5c1f8
charliedeus <charliedeus@gmail.com>
2022-08-18 15:03:03 -0300

    fix a cdatabase connection

--- config/database.js
+++ config/database.js
@@ -1,24 +1,12 @@
 module.exports = ({ env }) => ({
-  // connection: {
-  //   client: 'postgres',
-  //   connection: {
-  //     host: env('DATABASE_HOST', 'localhost'),
-  //     port: env.int('DATABASE_PORT', 5432),
-  //     database: env('DATABASE_NAME', 'agerba-db'),
-  //     user: env('DATABASE_USERNAME', 'postgres'),
-  //     password: env('DATABASE_PASSWORD', '852456951357'),
-  //     ssl: env.bool('DATABASE_SSL', false),
-  //   },
-  // },
-
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'agerba-db'),
-      user: env('DATABASE_USERNAME', 'docker'),
-      password: env('DATABASE_PASSWORD', 'docker'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', '852456951357'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
