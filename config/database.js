355cb2d341b3c4bc0b1f1ad873ee2d1468046f3d
charliedeus <charliedeus@gmail.com>
2022-08-25 10:10:19 -0300

    clean cache build

--- config/database.js
+++ config/database.js
@@ -11,5 +11,15 @@ module.exports = ({ env }) => ({
     },
   },
 
-
+  // connection: {
+  //   client: 'postgres',
+  //   connection: {
+  //     host: env('DATABASE_HOST', '127.0.0.1'),
+  //     port: env.int('DATABASE_PORT', 5432),
+  //     database: env('DATABASE_NAME', 'agerba-db'),
+  //     user: env('DATABASE_USERNAME', 'docker'),
+  //     password: env('DATABASE_PASSWORD', 'docker'),
+  //     ssl: env.bool('DATABASE_SSL', false),
+  //   },
+  // },
 });
