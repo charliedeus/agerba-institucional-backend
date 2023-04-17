26e745a7c013af8df948d1e3bf1ad1b19f829cd9
Charles Loureiro de Deus <charliedeus@gmail.com>
2023-04-17 14:05:04 -0300

    Add component files to news

---
+++ src/api/linha/controllers/linha.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * linha controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::linha.linha');
---
+++ src/api/linha/routes/linha.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * linha router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::linha.linha');
